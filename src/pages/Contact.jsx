d8dfbedbc22028416a70561c5294fa24f04da6f3
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-26 16:28:33 -0500

    added form to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div>
+            <form class="form-row">
+                <div className="form-group col-md-3">
+                    <label htmlFor="exampleFormControlTextarea1">Name</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+                </div>
+                <div className="form-group col-md-3">
+                    <label htmlFor="exampleFormControlInput1">Email address</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className="form-group col-md-3">
+                    <label htmlFor="exampleFormControlTextarea1">Message</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+                </div>
+            </form>
+            <input class="btn btn-primary" type="submit" value="Submit"></input>
+
+        </div>
     )
 }
 
