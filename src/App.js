d0d1a2cdb24c5d996684d6206fafa8f63d2dcf5c
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-25 20:15:06 -0500

    imported files into app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Navbar from './components/Navbar';
+import Project from './components/Project';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('About');
+
+  function renderPage() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
