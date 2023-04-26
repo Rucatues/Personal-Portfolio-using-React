5035921511f0ee6451e3ab5e804f5dbe86c18952
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-26 08:38:51 -0500

    increased size of react icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,18 @@ import { BsLinkedin } from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        // <div class="fixed-bottom">
-        //     <footer class="navbar navbar-light bg-light">
-        //         <a class="navbar-brand text-center" href="https://github.com/Rucatues">
-        //             <BsGithub />
-        //         </a>
-        //         <a class="navbar-brand text-right" href="https://www.linkedin.com/in/kelly-m-592a2645/">
-        //             < BsLinkedin />
-        //         </a>
-        //     </footer>
-        // </div>
         <div class="container fixed-bottom">
             <footer class="d-flex flex-wrap justify-content-end py-3 my-4 border-top">
                 <div class="d-flex">
                     <ul class="nav d-flex">
                         <li class="ms-3">
                             <a class="text-muted" href="https://github.com/Rucatues">
-                                <BsGithub />
+                                <BsGithub size="2em" />
                             </a>
                         </li>
                         <li class="ms-3">
                             <a class="text-muted" href="https://www.linkedin.com/in/kelly-m-592a2645/">
-                                < BsLinkedin />
+                                < BsLinkedin size="2em" />
                             </a>
                         </li>
                     </ul>
