d0a2140b875068d63a141f20a83db543c44250b1
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-26 16:31:08 -0500

    jumbotron from bootstrap added to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,11 @@ import { BsFillBootstrapFill } from 'react-icons/bs';
 
 const About = () => {
     return (
-        <div>About
+        <div>
+            <div class="jumbotron">
+                <h1 class="display-4">Welcome!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            </div>
         </div>
     )
 }
