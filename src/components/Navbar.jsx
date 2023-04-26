cf17cb9f3334106a8df41796a037d8bf3e4b6fa5
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-26 07:46:43 -0500

    bootstrap navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,26 @@ import React from 'react'
 const Navbar = ({ setCurrentPage, currentPage }) => {
     return (
         <div>
-            <a onClick={() => setCurrentPage('About')}>About</a>
-            <a onClick={() => setCurrentPage('Contact')}>Contact</a>
-            <a onClick={() => setCurrentPage('Portfolio')}>Portfolio</a>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Kelly</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" onClick={() => setCurrentPage('About')}>About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" onClick={() => setCurrentPage('Contact')}>Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" onClick={() => setCurrentPage('Portfolio')}>Portfolio</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
