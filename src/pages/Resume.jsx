31b4e117c0237381aa116d3fb14af0cbfc427c99
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-29 10:19:45 -0500

    downloadable resume added

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import resume from '/Users/kellymclain/bootcamp/Homework/Personal-Portfolio-using-React/src/components/images/resume.png'
 
 function Resume() {
     return (
-        <div>Resume</div>
+        <div className="images hero-image">
+            <a href={resume} download> <img src={resume} /> </a>
+        </div>
     )
 }
 
