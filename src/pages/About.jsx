87833f32501ea8e321daaaa17087a9aa954bb3ce
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-04-29 11:36:25 -0500

    text added to About page and footer fixed to bottom

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <div className="jumbotron">
-                <h1 className="display-4">Welcome!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <div className="col-md-6">
+                <div className="hero">
+                    <h1 className="hero-inner">
+                        Hi, my name is <span className="name">Kelly</span> and I am a web
+                        developer based in Austin, TX.
+                    </h1>
+                </div>
+
+                <p className="about">
+                    After a decade of working as a <a href="https://en.wikipedia.org/wiki/Maternal%E2%80%93fetal_medicine" class="MFM">Maternal Fetal Medicine</a> nurse, I made
+                    the decision to discover the world of web development. Working in telemedicine and using inaccessible and archaic EMR software sparked my interest to learn more about how websites and software are built and designed.
+                    My dream is to one day combine my knowledge of healthcare and technology to design an app that pulls data from a patient's chart and can triage their current issue without the need to call a provider's office. I am now
+                    looking for a junior dev position to kickstart my career and I am ready to learn among professionals!
+                </p>
             </div>
 
         </div>
